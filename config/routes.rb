003be0357968a144f8a2bfb9aa8de0f450ed4262
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-02-22 21:00:33 +0200

    add main: & reports:  controllers & view & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "reports/index"
+  get "reports/report_by_category"
+  get "reports/report_by_dates"
+  get "main/index"
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
