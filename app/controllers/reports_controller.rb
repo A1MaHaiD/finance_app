817c435fb0b4985f438c8ac6c7c36fada6de9fbf
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-02-25 12:14:37 +0200

    fix some chages

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,8 +3,12 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
+    @operations = Operation.where("category = ?", params[:category])
+    # Логіка для формування звіту по категоріям
   end
 
   def report_by_dates
+    @operations = Operation.where("date >= ? AND <= ?", params[:start_date], params[:end_date])
+    # Логіка для формування звіту по датам
   end
 end
