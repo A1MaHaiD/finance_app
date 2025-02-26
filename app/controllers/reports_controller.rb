a9e793ca125172f392b8426a18336b8dabf0f65f
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-02-26 17:07:42 +0200

    add category and dates report graph

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,18 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
-    @operations = Operation.where("category = ?", params[:category])
+    # @operations = Operation.where("category = ?", params[:category])
     # Логіка для формування звіту по категоріям
+    report_by_category = Operation.all.map { |report| [report.amount, report.category_id.to_s] }
+    @amounts = report_by_category.map { |r| r[0]}
+    @category_id = report_by_category.map { |r| r[1] }
   end
 
   def report_by_dates
-    @operations = Operation.where("date >= ? AND <= ?", params[:start_date], params[:end_date])
+    # @operations = Operation.where("date >= ? AND <= ?", params[:start_date], params[:end_date])
     # Логіка для формування звіту по датам
+    report_by_dates = Operation.all.map { |report| [report.amount, report.odate.to_s] }
+    @amounts = report_by_dates.map { |r| r[0]}
+    @odates = report_by_dates.map { |r| r[1] }
   end
 end
