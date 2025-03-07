80df55d22c3fd68b4c1a3f80e089baf3c71152e0
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-03-07 22:32:00 +0200

    some fix with button success & fix trouble DB

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[show edit update destroy]
 
   # GET /categories or /categories.json
   def index
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.html { redirect_to @category, notice: "Категорія була успішно створена." }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Категорія успішно збережена." }
+        format.html { redirect_to @category, notice: "Категорія успішно оновлена." }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,22 +49,24 @@ class CategoriesController < ApplicationController
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
-    @category.destroy!
-
     respond_to do |format|
-      format.html { redirect_to categories_path, status: :see_other, notice: "Категорія успішно видалена." }
-      format.json { head :no_content }
+      if @category.destroy
+        format.html { redirect_to categories_path, status: :see_other, notice: "Категорія успішно видалена." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to @category, alert: @category.errors.full_messages.join('. ') }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :description)
-    end
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,15 @@ class Category < ApplicationRecord
     paginates_per 10
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
+
+    before_destroy :check_for_operations
+
+    private
+
+    def check_for_operations
+        if operations.exists?
+            errors.add(:base, 'Неможливо видалити категорію, поки існують пов’язані операції.')
+            throw :abort
+        end
+    end
 end
