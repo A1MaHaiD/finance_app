85ef3385a72f953b3641f547e70007f9b12995c1
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-03-13 18:50:37 +0200

    rollback models tests

--- test/models/operation_test.rb
+++ test/models/operation_test.rb
@@ -6,7 +6,6 @@ class OperationTest < ActiveSupport::TestCase
   category_first = Category.find_by(name: "1st_Category")
 
   test "return true if everything is good" do
-    category_sixth = Category.find_by(name: "6th Category")
     # When
     operation_new = Operation.new(
       amount: 1.09,
@@ -201,7 +200,6 @@ class OperationTest < ActiveSupport::TestCase
   end
 
   test "should save operation with category" do
-    category_first = Category.find_by(name: "1st_Category")
     operation = Operation.new(
       amount: 9.99,
       operation_type: "Витрати",
