67686df35b41525f6f72d0f8b09004f6764cde70
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-03-15 10:40:22 +0200

    add def::check_database_and_redirect to main_controller.rb & update def::index & update routes.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,23 @@
 class MainController < ApplicationController
   def index
+    if params[:firstname].present? && params[:lastname].present? && params[:password].present?
+      # Користувач натиснув кнопку "Увійти", перевіряємо дані в базі
+      check_database_and_redirect
+    else
+      # Звичайний виклик методу index до натискання кнопки
+      render :index
+    end
+  end
+
+  private
+
+  def check_database_and_redirect
+    if Category.none?
+      redirect_to categories_path, alert: "Додайте хоча б одну категорію, щоб продовжити."
+    elsif Operation.none?
+      redirect_to category_operations_path(Category.first), alert: "Додайте хоча б одну операцію, щоб продовжити."
+    else
+      redirect_to reports_path, notice: "Ласкаво просимо до генератора звітів!"
+    end
   end
 end
