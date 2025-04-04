270211246cd71229fe5e0b5637ca1c0210450a37
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-04-04 20:10:52 +0300

    complete request report_by_dates.html.erb & report_by_category.html.erb & reports_controller.rb & some fix _header.html.erb & index.html.erb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,13 +25,19 @@ class ReportsController < ApplicationController
       flash[:alert] = "Будь ласка, заповніть поля початкової та кінцевої дати."
       redirect_to reports_path and return
     end
+    if params[:category].blank?
+      flash[:alert] = "Будь ласка, виберіть категорію."
+      redirect_to reports_path and return
+    end
     # Логіка для формування звіту по датам
     @report_by_dates = current_user.operations.joins(:category)
                                    .where('odate BETWEEN ? AND ?', params[:start_date], params[:end_date])
                                    .where(operation_type: params[:operation_type])
+                                   .where(categories: { id: params[:category] })
                                    .group("odate")
                                    .sum(:amount)
-    @odates = @report_by_dates.keys.map(&:to_s)
+    @odates = @report_by_dates.keys.map { |date| date.strftime("%d-%m-%Y") }
     @amounts = @report_by_dates.values
+    @categories = { id: params[:category], name: Category.find(params[:category]).name }
   end
 end
