8440010e4749afc3edb15b2999f67635d78fe4dd
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-05-12 20:45:48 +0300

    delete old migration and schemas

--- db/migrate/20250424115433_change_amount_scale_in_operations.rb
+++
@@ -1,9 +0,0 @@
-class ChangeAmountScaleInOperations < ActiveRecord::Migration[8.0]
-  def up
-    change_column :operations, :amount, :decimal, precision: 10, scale: 2
-  end
-
-  def down
-    change_column :operations, :amount, :decimal, precision: 10, scale: 0
-  end
-end
