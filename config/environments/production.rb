b7d66cae995d60709f20f37fd68b614ec0054a63
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-05-28 00:18:37 +0300

    add ENV["SMTP_USER_NAME" & SMTP_PASSWORD]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            Rails.application.credentials.dig(:smtp, :user_name),
-    password:             Rails.application.credentials.dig(:smtp, :password),
+    user_name:            Rails.application.credentials.dig(:smtp, ENV["SMTP_USER_NAME"]),
+    password:             Rails.application.credentials.dig(:smtp, ENV["SMTP_PASSWORD"]),
     authentication:       :plain,
     enable_starttls_auto: true
   }
