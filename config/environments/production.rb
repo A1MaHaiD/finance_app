fd19dccf20699d7cfa933d1017c1a74ae05dc5d1
A1MaHaiD <super.lukianchuk2010@gmail.com>
2025-05-31 14:24:07 +0300

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            Rails.application.credentials.dig(:smtp, ENV["SMTP_USER_NAME"]),
-    password:             Rails.application.credentials.dig(:smtp, ENV["SMTP_PASSWORD"]),
-    authentication:       "plain",
+    user_name:            Rails.application.credentials.dig(:smtp, :user_name),
+    password:             Rails.application.credentials.dig(:smtp, :password),
+    authentication:       :plain,
     enable_starttls_auto: true
   }
   config.action_mailer.perform_deliveries = true
